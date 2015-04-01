653344465bf396cda7b56ff9b21380b9b2785dc6
Scott <scott@sscotth.io>
2015-04-01 16:10:16 -0500

    starter contact list

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
@@ -10,44 +6,113 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
+  ListView
 } = React;
 
+var API_URL = 'http://api.randomuser.me/?results=100';
+
 var AwesomeProject = React.createClass({
-  render: function() {
+
+  getInitialState: function () {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      loaded: false
+    };
+  },
+
+  componentDidMount: function () {
+    this.fetchData();
+  },
+
+  fetchData: function () {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(data.results),
+          loaded: true
+        })
+      })
+      .done();
+  },
+
+  renderLoadingView: function () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Shift+Z for dev menu
+        <Text>
+          Loading client list...
         </Text>
       </View>
     );
+  },
+
+  renderClientRow: function (data) {
+    var client = data.user;
+
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: client.picture.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <Text style={styles.name}>
+          {this.capitalizeString(client.name.first)} {this.capitalizeString(client.name.last)}
+        </Text>
+        <Text style={styles.infoIcon}>
+          {'info'}
+        </Text>
+      </View>
+    );
+  },
+
+  capitalizeString: function (s) {
+    return s[0].toUpperCase() + s.slice(1).toLowerCase();
+  },
+
+  render: function () {
+
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderClientRow}
+        style={styles.listView}
+      />
+    )
   }
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  listView: {
+    paddingTop: 20,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  name: {
+    flex: 2,
+    fontSize: 24
+  },
+  infoIcon: {
+    flex: 0
+  },
+  thumbnail: {
+    width: 50,
+    height: 50,
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    borderRadius: 25
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
